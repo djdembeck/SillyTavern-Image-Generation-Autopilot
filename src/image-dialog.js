fcc7a172c71497d448feb85382a43156c4b33ca5
djdembeck <dembeck.david@protonmail.com>
2026-04-10 18:22:22 -0500

    chore: remove debug logging, cancel is working

--- src/image-dialog.js
+++ src/image-dialog.js
@@ -1128,20 +1128,12 @@ export class ImageSelectionDialog {
     }
 
     _handleCancel() {
-        const isGenerating = this.isGenerating;
-        console.log('[ImageSelectionDialog] Cancel clicked', { isGenerating });
-        
-        if (isGenerating) {
-            console.log('[ImageSelectionDialog] Aborting generator');
+        if (this.isGenerating) {
             this.generator.abort();
-            
-            // Stop SD generation via custom event
             const ctx = typeof SillyTavern !== 'undefined' && typeof SillyTavern.getContext === 'function'
                 ? SillyTavern.getContext()
                 : null;
-            console.log('[ImageSelectionDialog] Context for eventSource:', { hasCtx: !!ctx, hasEventSource: !!ctx?.eventSource });
             if (ctx?.eventSource && typeof ctx.eventSource.emit === 'function') {
-                console.log('[ImageSelectionDialog] Emitting sd_stop_generation');
                 ctx.eventSource.emit('sd_stop_generation');
             }
         }
